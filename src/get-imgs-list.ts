86c2e28f77b0559b2209bc643fa111e4b61d18ea
John Bucknam <jbuck594@gmail.com>
2023-05-20 19:16:10 -0400

    Added recursive image search to image gallery

--- src/get-imgs-list.ts
+++ src/get-imgs-list.ts
@@ -4,7 +4,7 @@ import renderError from './render-error'
 const getImagesList = (
     app: App,
     container: HTMLElement,
-    settings: {[key: string]: any}
+    settings: {[key: string]: any},
   ) => {
   // retrieve a list of the files
   const folder = app.vault.getAbstractFileByPath(settings.path)
@@ -19,9 +19,19 @@ const getImagesList = (
 
   // filter the list of files to make sure we're dealing with images only
   const validExtensions = ["jpeg", "jpg", "gif", "png", "webp", "tiff", "tif"]
-  const images = files.filter(file => {
+  let images = files.filter(file => {
     if (file instanceof TFile && validExtensions.includes(file.extension)) return file
   })
+
+  // check if we should recursively look for files in sub-folders, if so find all files.
+  if (settings.recursive) {
+    files.forEach(f => {
+      if (f instanceof TFolder && f.path != settings.path) {
+        settings.path = f.path;
+        images = images.concat(getImagesListHelper(app, container, settings, f.path))
+      }
+    })
+  }
 
   // sort the list by name, mtime, or ctime
   const orderedImages = images.sort((a: any, b: any) => {
@@ -34,13 +44,45 @@ const getImagesList = (
   const sortedImages = settings.sort === 'asc' ? orderedImages : orderedImages.reverse()
 
   // return an array of objects
-  return sortedImages.map(file => {
-    return {
-      name: file.name,
-      folder: file.parent.path,
-      uri: app.vault.adapter.getResourcePath(file.path)
-    }
+  return sortedImages.map(file => { return {
+    name: file.name,
+    folder: file.parent.path,
+    uri: app.vault.adapter.getResourcePath(file.path)
+    }})
+}
+
+const getImagesListHelper = (
+  app: App,
+  container: HTMLElement,
+  settings: {[key: string]: any},
+  path: string
+) => {
+
+  // retrieve a list of the files
+  const folder = app.vault.getAbstractFileByPath(path)
+
+  let files
+  if (folder instanceof TFolder) { files = folder.children }
+  else {
+    return []
+  }
+
+  // filter the list of files to make sure we're dealing with images only
+  const validExtensions = ["jpeg", "jpg", "gif", "png", "webp", "tiff", "tif"]
+  let images = files.filter(file => {
+    if (file instanceof TFile && validExtensions.includes(file.extension)) return file
   })
+
+  // check if we should recursively look for files in sub-folders, if so find all files.
+  if (settings.recursive) {
+    files.forEach(f => {
+      if (f instanceof TFolder && f.path != path) {
+        images = images.concat(getImagesListHelper(app, container, settings, f.path))
+      }
+    })
+  }
+
+  return images
 }
 
 export default getImagesList
